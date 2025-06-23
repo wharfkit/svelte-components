58ade5a4f134d3988e64de4f0b37adcbc8f2bb45
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-06-23 16:07:41 -0700

    feat: add skeleton loader

---
+++ src/lib/components/loading/Skeleton.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { cn } from '$lib/utils';
+
+	let props = $props();
+</script>
+
+<div
+	class={cn('bg-surface-container-high min-h-4 min-w-32 animate-pulse rounded-md', props.class)}
+></div>
--- src/routes/sections/loading.svelte
+++ src/routes/sections/loading.svelte
@@ -1,10 +1,17 @@
 <script>
-	import { Progress, Stack } from '$lib';
+	import { Cluster, Progress, Stack } from '$lib';
+	import Skeleton from '$lib/components/loading/Skeleton.svelte';
 </script>
 
 <Stack tag="section" id="loading">
 	<h2 class="text-headline">Loading</h2>
-	<h3 class="text-title">Progress</h3>
 
+	<h3 class="text-title">Progress Indicator</h3>
 	<Progress percentage={20} />
+
+	<h3 class="text-title">Skeleton Loader</h3>
+	<Cluster>
+		<Skeleton />
+		<Skeleton />
+	</Cluster>
 </Stack>
